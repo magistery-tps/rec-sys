00a642399bfaecfc5887367eb93dee53c53a3a88
adrianmarino <adrianmarino@gmail.com>
2023-08-16 19:28:07 -0300

    feat: setup default recommenders max similar users & items

--- recsys/recsysweb/models/recommender.py
+++ recsys/recsysweb/models/recommender.py
@@ -40,9 +40,9 @@ class Recommender(models.Model):
         null         = True,
         blank        = True
     )
-    max_similar_users         = models.IntegerField(default = 0, verbose_name = 'Max Similar Users')
-    max_items_by_similar_user = models.IntegerField(default = 0, verbose_name = 'Max Items by Similar Users')
-    max_similar_items         = models.IntegerField(default = 0, verbose_name = 'Max Similar Items')
+    max_similar_users         = models.IntegerField(default = 10, verbose_name = 'Max Similar Users')
+    max_items_by_similar_user = models.IntegerField(default = 20, verbose_name = 'Max Items by Similar Users')
+    max_similar_items         = models.IntegerField(default = 50, verbose_name = 'Max Similar Items')
     position = models.IntegerField(default = 1, verbose_name = 'Position in Recommendations View')
     enable = models.BooleanField(default = True, verbose_name = 'Enable')
 
